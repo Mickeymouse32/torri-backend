f5e5bf5964e9d6316ce88fcc13f751a8a39d586b
mickeymouse32 <mick8968@icloud.com>
2025-05-15 03:42:21 -0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -88,4 +88,49 @@ const handleVerifyEmail = async (req, res) => {
   }
 };
 
-module.exports = { handleRegister, handleVerifyEmail };
+//handle login
+
+const handleLogin = async (req, res) => {
+  const { email, password, role } = req.body;
+  if (!email || !password || !role) {
+    return res
+      .status(400)
+      .json({ message: "Email, password, and role are required" });
+  }
+  try {
+    const user = await USER.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Account Not found, Register" });
+    }
+    if (user.role !== role) {
+      return res.status(403).json({ message: "Access Denied for this role" });
+    }
+    if (!user.isVerified) {
+      return res
+        .status(403)
+        .json({ message: "Email Not verified, Check your mail" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid email or Password" });
+    }
+
+    //generate a token
+
+    return res.status(200).json({
+      success: true,
+      user: {
+        fullName: user.fullName,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { handleRegister, handleVerifyEmail, handleLogin };
